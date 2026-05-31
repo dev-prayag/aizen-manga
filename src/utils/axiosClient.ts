7fc5891791d3c537b68db854c747e78a8d263e39
Prayag <prayagfx@gmail.com>
2026-05-31 22:22:41 +0530

    add src/utils/axiosClient.ts

---
+++ src/utils/axiosClient.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+  export const client = axios.create({
+    baseURL: process.env.BASE_URL || 'https://mangafire.to',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36',
+      Referer: 'https://mangafire.to/',
+    },
+  });
+  
